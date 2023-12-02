ae2fc9bd9113af50cac066505bb90497f460d2e1
K7NGhost <Kevinargueta05@gmail.com>
2023-12-02 15:55:13 -0500

    foundation of program

---
+++ tasks.py
@@ -0,0 +1,17 @@
+from opcode import opname
+
+
+class task:
+    def __init__(self, task_lists=[], priority='low priority'):
+        self.task_lists = task_lists
+        self.priority = priority
+        
+    def add(self, task):
+        self.task_lists.append(task)
+        with open('text.txt', 'w') as file:
+            for items in self.task_lists:
+                file.write(items + '\n')
+    
+    def printInfo(self):
+        print(f'The tasks in the list are {self.task_lists}')
+        print(f'The priority of these tasks are {self.priority}')
