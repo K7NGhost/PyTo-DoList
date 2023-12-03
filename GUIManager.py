64aa1ebf9fbbfab1c97f807fcdd1c38f4b7ad439
K7NGhost <Kevinargueta05@gmail.com>
2023-12-03 01:31:36 -0500

    added check marks
    complete button now works / able to mark tasks as done

--- GUIManager.py
+++ GUIManager.py
@@ -23,7 +23,7 @@ class GUIManagers:
         remove_button = Button(self.main_frame, text="remove", fg='black', command=self.remove_screen, width=self.button_width, height=self.button_height)
         remove_button.pack(side=TOP, pady=10)
 
-        complete_button = Button(self.main_frame, text="mark complete", fg='black', command=self.show_alternate_screen, width=self.button_width, height=self.button_height)
+        complete_button = Button(self.main_frame, text="mark complete", fg='black', command=self.complete_screen, width=self.button_width, height=self.button_height)
         complete_button.pack(side=TOP, pady=10)
 
         clear_button = Button(self.main_frame, text="clear tasks", fg='black', command=someTask.clear, width=self.button_width, height=self.button_height)
@@ -60,6 +60,20 @@ class GUIManagers:
         back_button = Button(remove_frame, text="Back", command=self.show_main_screen)
         back_button.pack(pady=10)
 
+    def complete_screen(self):
+        self.main_frame.destroy()
+        complete_frame = Frame(self.root)
+        complete_frame.pack(fill=BOTH, expand=True)
+        label = Label(complete_frame, text="Enter index of task to mark done")
+        label.pack(pady=20)
+        self.index_entry = Entry(complete_frame, width=10)
+        self.index_entry.pack(pady=10)
+
+        save_button = Button(complete_frame, text="Save", command=self.complete_index)
+        save_button.pack(pady=10)
+        back_button = Button(complete_frame, text="Back", command=self.show_main_screen)
+        back_button.pack(pady=10)
+
     def show_main_screen(self):
         self.clear_all_frames()
         self.root.geometry("400x300")
@@ -71,7 +85,7 @@ class GUIManagers:
         remove_button = Button(self.main_frame, text="remove", fg='black', command=self.remove_screen, width=self.button_width, height=self.button_height)
         remove_button.pack(side=TOP, pady=10)
 
-        complete_button = Button(self.main_frame, text="mark complete", fg='black', command=self.show_alternate_screen, width=self.button_width, height=self.button_height)
+        complete_button = Button(self.main_frame, text="mark complete", fg='black', command=self.complete_screen, width=self.button_width, height=self.button_height)
         complete_button.pack(side=TOP, pady=10)
 
         clear_button = Button(self.main_frame, text="clear tasks", fg='black', command=someTask.clear, width=self.button_width, height=self.button_height)
@@ -90,3 +104,8 @@ class GUIManagers:
         index_num = int(index_text)
         print('num: ', index_num)
         someTask.remove(index_num)
+    def complete_index(self):
+        index_text = self.index_entry.get()
+        print(index_text)
+        index_num = int(index_text)
+        someTask.mark_done(index_num)
--- tasks.py
+++ tasks.py
@@ -18,33 +18,57 @@ class task:
         #if header does not exist create it
         if not header_exists:
             with open('text.txt', 'a') as file:
-                file.write(f"{'Index':^15}{'Task':^20}{'Priority':^15}{'Date Added':^20}\n")
+                file.write(f"{'Index':^10}{'Task':^20}{'Priority':^15}{'Date Added':^20}{'Check':^10}\n")
 
+    #grabs the last index in the list
     def get_last_index(self):
         with open('text.txt', 'r') as file:
+            # Read the header line
             file.readline()
-            last_line = file.readlines()[-1]
-            last_index = int(last_line.split()[0])
-            return last_index
         
+            # Read the remaining lines and store in a list
+            lines = file.readlines()
+
+            # Check if there are any lines in the file
+            if lines:
+                # Get the last line and extract the index
+                last_line = lines[-1]
+                last_index = int(last_line.split()[0])
+                return last_index
+            else:
+                # If there are no lines, return 0
+                return 0
+    
+    #Add task to the list
     def add(self, task, priority='low priority'):
         date_added = datetime.now().strftime('%d-%m-%Y')
         self.task_lists.append({'task': task, 'priority':priority, 'date_added': date_added})
         with open('text.txt', 'a') as file:
-            file.write(f"{self.counter:^5}{task:^20}{priority:^15}{date_added:^20}\n")
+            file.write(f"{self.counter:^10}{task:^20}{priority:^15}{date_added:^20}{' ':^10}\n")
         self.counter += 1
 
+    #remove a task based on the index num
     def remove(self, index):
         if 1 <= index <= len(self.task_lists):
             removed_task = self.task_lists.pop(index - 1)
 
             with open('text.txt', 'w') as file:
-                file.write(f"{'Index':^10}{'Task':^20}{'|Priority':^15}{'|Date Added':^20}\n")
+                file.write(f"{'Index':^10}{'Task':^20}{'Priority':^15}{'Date Added':^20}{'Check':^10}\n")
                 for i, task_info in enumerate(self.task_lists, start=1):
-                    file.write(f"{i:^10}{task_info['task']:^20}{task_info['priority']:^15}{task_info['date_added']:^20}\n")
+                    file.write(f"{i:^10}{task_info['task']:^20}{task_info['priority']:^15}{task_info['date_added']:^20}{' ':^10}\n")
             return removed_task
         else:
             print("Invalid index. Please provide a valid index")
+    
+    def mark_done(self, index):
+        if 1 <= index <= len(self.task_lists):
+            self.task_lists[index - 1]['check'] = '✓'
+
+            with open('text.txt', 'w', encoding='utf-8') as file:
+                file.write(f"{'Index':^10}{'Task':^20}{'Priority':^15}{'Date Added':^20}{'Check':^10}\n")
+                for i, task_info in enumerate(self.task_lists, start=1):
+                    file.write(f"{i:^10}{task_info['task']:^20}{task_info['priority']:^15}{task_info['date_added']:^20}"
+                               f"{task_info.get('check', ' '):^10}\n")
 
     def printInfo(self):
         print(f'The tasks in the list are {self.task_lists}')
@@ -55,5 +79,5 @@ class task:
         with open('text.txt', 'w') as file:
             pass
         with open('text.txt', 'a') as file:
-            file.write(f"{'Index':^5}{'Task':^20}{'Priority':^15}{'Date Added':^20}\n")
+            file.write(f"{'Index':^10}{'Task':^20}{'Priority':^15}{'Date Added':^20}{'Check':^10}\n")
         self.counter = 1
